097873eb174f343215aaca7705c355e384c120f7
tzh <2375626546@qq.com>
2018-08-10 18:01:56 +0800

    增加测试

--- livecode/apis/user_img.py
+++ livecode/apis/user_img.py
@@ -2,4 +2,15 @@ from aiohttp import web
 from bson import ObjectId
 
 from ..lib.utils import get_file_url
-from ..validate import
+from ..validate import *
+
+
+class UserImg(web.View):
+    async def post(self):
+        open_id = self.request['open_id']
+        img = self.request['body'].getall('img', [])
+
+        if not img:
+            return web.json_response({'errcode': 0}, )
+
+
